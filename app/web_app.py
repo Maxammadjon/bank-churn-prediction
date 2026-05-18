862d786cde30acef57978bf07a1ad155b752dd53
Makhamadjon Shopaizov <makhamadjonshopaizov@gmail.com>
2026-05-19 01:13:18 +0500

    fix render port

--- app/web_app.py
+++ app/web_app.py
@@ -83,7 +83,7 @@ def func_predict():
 
 if __name__ == '__main__':
     app.run(
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 10000)),
-        debug=True
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 10000)),
+        debug=False
     )
